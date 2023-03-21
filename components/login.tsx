a11165e470b12b5aaf839e9230eab4c7fe91f9d9
wobble-house <ross@wobble.house>
2023-03-21 16:01:56 -0400

    updating login button

--- components/login.tsx
+++ components/login.tsx
@@ -6,15 +6,16 @@ export default function LoginButton() {
     if (status === "authenticated") {
       return (
         <>
-              <div className="flex ml-3 pt-2 hover:scale-105">
+        <div className="flex flex-col max-w-lg">
+          <p className="text-black">Hi, {session.user.name}!</p>
+          <div className="flex ml-3 pt-2 hover:scale-105">
       <div className="bg-rosspurple pr-1 pb-1 mt-1 pt-1">
-      <div className="bg-rossblue pr-1 pb-1 -ml-1 -mt-1"></div>
-        <div className="flex flex-row max-w-lg">
-          <p className="text-white">Hi, {session.user.name}!</p>
-          <button className=" text-white bg-rosspurple px-4 py-1 -ml-1 -mt-1" onClick={() => signOut()}>Sign out</button>
-          </div>
-          </div>
+      <div className="bg-rossblue pr-1 pb-1 -ml-1 -mt-1">
+        <button className=" text-white bg-rosspurple px-4 py-1 -ml-1 -mt-1" onClick={() => signOut()}>Sign out</button>
         </div>
+        </div>
+      </div>
+          </div>
         </>
       )
     }
