0abcbe8b1cc7a7d40df6bcbacd956db66b5e8e83
wobble-house <ross@wobble.house>
2023-03-21 16:10:36 -0400

    updating login button again

--- components/login.tsx
+++ components/login.tsx
@@ -7,7 +7,7 @@ export default function LoginButton() {
       return (
         <>
         <div className="flex flex-col max-w-lg">
-          <p className="text-black">Hi, {session.user.name}!</p>
+          <p className="text-black font-bold pl-1">Hi, {session.user.name}!</p>
           <div className="flex ml-3 pt-2 hover:scale-105">
       <div className="bg-rosspurple pr-1 pb-1 mt-1 pt-1">
       <div className="bg-rossblue pr-1 pb-1 -ml-1 -mt-1">
