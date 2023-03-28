af0f83f133114eea5061546a795cb33d5e706d20
wobble-house <ross@wobble.house>
2023-03-28 16:56:36 -0400

    fixing client error

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,6 +1,4 @@
 'use client';
-import { GAScript, GABody } from './consent'
-import { CookieAccept } from '../components/cookie-consent'
 import Footer from './footer'
 import './globals.css'
 import AuthContext from '../components/next-auth-provider'
@@ -8,20 +6,6 @@ import ThemeToggle from '../components/theme-switcher'
 import MyNavbar from '../components/navbar'
 import ChatModal from '../components/modal'
 import LoginButton from '../components/login'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
-library.add(fas, faFontAwesome)
-
-export const dynamic = 'force-dynamic',
-  dynamicParams = true,
-  revalidate = 0,
-  fetchCache = 'auto',
-  runtime = 'nodejs',
-  preferredRegion = 'auto'
 
 export const metadata = {
     title: 'Error',
@@ -55,14 +39,11 @@ export default function GlobalError({
         href="/favicon/favicon-16x16.png"
       />
       <link rel="icon" href="/favicon/rossalanford.ico" />
-      <GAScript/>
 <body className="min-h-screen">
-      <GABody/>
       
         <AuthContext>
 
             <main className="md:flex flex-row max-w-7xl mx-auto gap-5 justify-evenly align-top">
-              <CookieAccept/>
                 <MyNavbar><ThemeToggle/></MyNavbar>
         <h2 className="text-center">Something went wrong!</h2>
         <button onClick={() => reset()}>Try again</button>
