27b9c35dbd9e5e95134a7379d110bec639fb3237
wobble-house <ross@wobble.house>
2023-11-02 03:48:14 -0400

    fixing mobile group hover

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export default function Logo(){
     return(
-        <div className="flex justify-center items-center mx-10 z-50 px-10 pb-12 md:pb-0 group-hover:animate-bounce group-hover:items-end">
+        <div className="flex justify-center items-center mx-10 z-50 px-10 pb-12 md:pb-0 group-hover:animate-bounce md:group-hover:items-end">
             <Image src={`/logos/RAF-logo.svg`} alt="Ross Alan Ford logo" width={100} height={100} className=" absolute z-30 drop-shadow-4xl"/>
         </div>
     )
