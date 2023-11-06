52e5340c7001c00dfc149eff2de62306e7db733c
wobble-house <ross@wobble.house>
2023-11-06 18:58:55 -0500

    I odn't know

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
 <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`} style={{scrollBehavior:'smooth'}}>
 <body className="relative max-w-screen overflow-y-auto no-scrollbar overscroll-auto pb-20">
-  <Image src={'/images/background.png'} alt='main background image' fill sizes="100vw" style={{objectFit: 'cover'}} className={`z-0`}/>
+  <Image src={'/images/background.png'} alt='main background image'fill style={{objectFit: 'cover'}} className={`top-0 left-0 z-0  `}/>
           {children}
           <Footer/>
         </body>
