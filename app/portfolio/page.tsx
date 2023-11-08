7f01a1144607ea07a9b06d409680a601ff1fedaf
wobble-house <ross@wobble.house>
2023-11-07 19:49:02 -0500

    adding projects to portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,18 +1,21 @@
+import 'server-only';
 import Header from "../../components/header";
 import Nav from "../../components/nav";
+import ProjectList from "../../components/projectList";
+import { projects } from "../../lib/data";
 
 export const metadata = {
   title: 'Portfolio',
   description: 'Here are some of the Projects I have worked on',
 }
 
-export default function Portfolio() {
-
+export default async function Portfolio() {
   return (
     <main className="flex flex-col mx-auto min-h-screen">
       <Nav/>
-      <div className=" flex-col px-8 pt-12 md:pt-8 pb-8 justify-center mx-auto ">
+      <div className="relative flex-col px-8 pt-12 md:pt-8 pb-8 justify-center mx-auto ">
         <Header metadata={metadata}/>
+        <ProjectList projects={projects}/>
       </div>
      </main>
   )
